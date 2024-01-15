244e4a8c80297e3700455640dbee246be3b48108
adityasethi06 <adityasethi199533@gmail.com>
2024-01-15 20:36:05 +0800

    handled cors policy

--- app/main.py
+++ app/main.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 from .database import engine
 from . import models
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# commenting below as alembic is handeling table creation
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(auth.router)
 app.include_router(post.router)
